baa98402b194ef96a20fdbfbc5cd5e503ebad30b
Lucian Bellini <lucianbellini1@gmail.com>
2023-05-25 15:42:39 -0300

    Adding put method and get specific item

--- dist/app.js
+++ dist/app.js
@@ -11,9 +11,21 @@ app.get("/", (req, res) => {
 app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 });
+app.get("/livros/:id", (req, res) => {
+    let index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
+});
 app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).send("Livro cadastrado com sucesso");
 });
+app.put("/livros/:id", (req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros);
+});
+function buscaLivro(id) {
+    return livros.findIndex((livro) => livro.id == id);
+}
 export default app;
 //# sourceMappingURL=app.js.map
